efb5bdb249ff694e38bd73e9c2b419971f6d28de
Ashutosh Maurya <proashu19@gmail.com>
2026-09-24 18:49:55 +0530

    done student search bar

--- frontend/src/Components/StudentsList.jsx
+++ frontend/src/Components/StudentsList.jsx
@@ -1,29 +1,56 @@
+import { useState } from 'react';
 import  ListItems from './ListItems';
 
 function StudentsList({ 
     students,
     onDeleteStudent,
    onUpdateStudent
- 
+   
 
- }) {
+ }) 
+ {
+    const [searchTerm , setSearchTerm] = useState ('');
+
+   const filteredStudents = students.filter((student) =>
+    student.name.toLowerCase().includes(searchTerm.toLowerCase())
+);
+
+
     return (
         <div className="panel student-panel">
             <h2>Students List</h2>
-            {students.length === 0 ? (
-                <p className="empty-state">No Student yet.</p>
-            ) : (
-                <ul className="student-list">
-                    {students.map((student) => (
-                        <ListItems
-                            student={student}
-                            key={student._id}
-                            onDeleteStudent={onDeleteStudent}
-                            onUpdateStudent={onUpdateStudent}
-                        />
-                    ))}
-                </ul>
-            )}
+            <input
+                type= "text"
+                value={searchTerm}
+                onChange={(event) => setSearchTerm(event.target.value)}
+                placeholder="Search by name..."
+                className="student-search"
+                />
+
+                {
+                    filteredStudents.length === 0 ?
+                    ( <p className= "empty-state">
+                         {students.length === 0 ? 'No Student yet.' : 'No matching students.'}
+                    </p>
+                    ) :(
+                        <ul className="student-list">
+                            {
+                                filteredStudents.map((student)=>(
+                                    <ListItems
+                                    student = {student}
+                                    key = {student._id}
+                                    onDeleteStudent={onDeleteStudent}
+                                    onUpdateStudent={onUpdateStudent}
+                                    />
+                                ))
+                            }
+                        </ul>
+                    )
+                }
+
+
+
+          
         </div>
     );
 }
